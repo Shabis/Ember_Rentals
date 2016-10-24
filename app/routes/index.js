411f97cf1ad415b59fe40fbe64beb2a9cb2a8915
Shelby Clayton and Vanessa Palacios Sharma <vanessa@>
2016-10-24 16:34:47 -0700

    create individual pages for each rental and add styling.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,4 @@
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
@@ -9,11 +10,6 @@ export default Ember.Route.extend({
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-
-    destroyRental(rental) {
-      rental.destroyRecord();
       this.transitionTo('index');
     }
   }
